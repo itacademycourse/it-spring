59889a5f175dc983f74f41257564779bcbd1942d
Maxim_Belov <belovmd@gmail.com>
2020-04-09 21:23:37 +0300

    Add details for each material

--- lesson/urls.py
+++ lesson/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 app_name = 'lesson'
 urlpatterns = [
-    path('', views.all_materials, name='all_materials')
+    path('', views.all_materials, name='all_materials'),
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>',
+         views.material_details,
+         name='material_details'),
 ]
--- lesson/views.py
+++ lesson/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from . import models
 # Create your views here.
@@ -9,3 +9,14 @@ def all_materials(request):
     return render(request,
                   'materials/all_materials.html',
                   {"materials": material_list})
+
+
+def material_details(request, year, month, day, slug):
+    material = get_object_or_404(models.Material,
+                                 slug=slug,
+                                 publish__year=year,
+                                 publish__month=month,
+                                 publish__day=day)
+    return render(request,
+                  'materials/detail.html',
+                  {'material': material})
