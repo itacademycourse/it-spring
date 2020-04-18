eb9c647987a2995cffb5274b593e36b77a238cb4
Maxim Belov <belovmd@gmail.com>
2020-04-18 15:15:10 +0300

    Add password reset

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 app_name = 'lesson'
+
+
+class MyHackedView(auth_views.PasswordResetView):
+    success_url = reverse_lazy('lesson:password_reset_done')
+
+
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
     # path('', views.MaterialListView.as_view(), name='all_materials'),
@@ -14,4 +22,11 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    # path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', MyHackedView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(success_url=reverse_lazy('lesson:password_reset_complete')),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
